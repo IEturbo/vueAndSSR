57dcf5e0282d5a620eed29246b1e5ce115015900
IEturbo <ghb_918@163.com>
2018-06-04 23:16:56 +0800

    实例

--- practice/instance/index.js
+++ practice/instance/index.js
@@ -4,7 +4,8 @@ const app = new Vue({
   // el: '#root',
   template: `<div ref="dddd">hello {{text}}</div>`,
   data: {
-    text: 0
+    text: 0,
+    obj: {}
   }
 })
 
@@ -12,15 +13,40 @@ app.$mount('#root')
 
 setInterval(() => {
   app.text += 1
-  app.$data.text += 1
+  // app.$data.text += 1
+  // app.obj.a = app.obj.text
+  app.$set(app.obj, 'a', 1)
+  // app.$forceUpdate()
 }, 1000)
 
-console.log(app.$options)
-console.log(app.$props)
-console.log(app.$el)
-console.log(app.$root)
-console.log(app.$children)
-console.log(app.$slots)
-console.log(app.$scopedSlots)
-console.log(app.$refs)
-console.log(app.$isServer) // 是否服务端渲染
+// console.log(app.$options)
+// console.log(app.$props)
+// console.log(app.$el)
+// console.log(app.$root)
+// console.log(app.$children)
+// console.log(app.$slots)
+// console.log(app.$scopedSlots)
+// console.log(app.$refs)
+// console.log(app.$isServer) // 是否服务端渲染
+// 下面的代码   当页面有变化的时候会发生变化
+// app.$options.render = (h) => {
+//   return h('div', {}, 'new render function')
+// }
+
+// 方法
+// $watch
+// app.$watch('text', (newData, oldData) => {
+//   console.log(`${newData}` + ':' + `${oldData}`)
+// })
+
+// app.$on('test', (a, b) => {
+//   console.log(`test emited ${a} ${b}`)
+// })
+
+// setInterval(() => {
+//   app.$emit('test', 1, 2)
+// }, 1000)
+
+setInterval(() => {
+  app.$forceUpdate()
+}, 1000)
